17065dc3c31e2e33ba3609e7e2afc7562b961818
pandeysudan27 <pandeysudan27@gmail.com>
2020-02-07 18:22:38 +0100

    Files generated by PkgTemplates

---
+++ src/OpenHPLJulia.jl
@@ -0,0 +1,5 @@
+module OpenHPLJulia
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using OpenHPLJulia
+using Test
+
+@testset "OpenHPLJulia.jl" begin
+    # Write your own tests here.
+end
