3d50da6a72a2ffe776bffea26a6111c87ccf2433
pandeysudan27 <pandeysudan27@gmail.com>
2020-02-17 01:40:59 +0100

    headrace and surge tank design added

--- src/OpenHPLJulia.jl
+++ src/OpenHPLJulia.jl
@@ -4,5 +4,5 @@ using DataStructures
 include("parameters.jl")
 include("fluid.jl")
 include("unitdesign.jl")
-export penstock, headrace, f2xy
+export penstock, headraceNsuretank
 end # module
--- src/unitdesign.jl
+++ src/unitdesign.jl
@@ -16,19 +16,38 @@ function penstock(Hn, Vdotn, H, theta; method="Bier",eta_nominal=0.9,pipe_roughn
                 "d. Nominal power output"=>P,
                 "e. Design based on"=>method,
                 "f. Economical diameter"=>D,
-                "g. Height"=>H,
-                "h. Length"=>L,
-                "i. Hydraulic crosssection"=>A,
-                "j. Angle of inclination"=>theta,
-                "k. Average velocity"=>v,
-                "l. Nominal Reynolds number"=>NRe,
-                "m. Darcy's friction factor"=>f,
-                "n. Nominal allowed head loss"=>hL,
-                "o. Hydraulic gradient"=>hGrad,
-                "p. Pipe roughness"=>pipe_roughness,
-                "q. Gradient angle"=>gradAngle)))
+                "g. Cross-section"=>A,
+                "h. Height"=>H,
+                "i. Length"=>L,
+                "j. Hydraulic crosssection"=>A,
+                "k. Angle of inclination"=>theta,
+                "l. Average velocity"=>v,
+                "m. Nominal Reynolds number"=>NRe,
+                "n. Darcy's friction factor"=>f,
+                "o. Nominal allowed head loss"=>hL,
+                "p. Hydraulic gradient"=>hGrad,
+                "q. Pipe roughness"=>pipe_roughness,
+                "r. Gradient angle"=>gradAngle)))
 end
 
-function headrace(Hn, Vdotn, H, theta; method="Bier",eta_nominal=0.9,pipe_roughness=0.05)
-    return penstock(Hn, Vdotn, H, theta)
+function headraceNsurgetank(Hn, Vdotn, H, theta, Hns; method="Bier",eta_nominal=0.9,pipe_roughness=0.05)
+    g,rho,mu=parameters()
+    headrace_dict = penstock(Hn, Vdotn, H, theta,method=method,eta_nominal=eta_nominal,pipe_roughness=pipe_roughness)
+    # surge tank depends on headrace tunnel so
+    n = 1.6 # safety factor for Thoma criteria
+    # Acessing data of headrace tunnel from dictionary
+    Dt,Lt,At,ft,vt = headrace_dict["f. Economical diameter","i. Length","g. Cross-section","n. Darcy's friction factor","l. Average velocity"]
+    # calculating headrace tunnel friction factor
+    beta = ft*Lt/(2*g*Dt)
+    As = n*(Lt*At)/(2*g*beta*(Hn-beta*vt^2))
+    Ds = sqrt(4*As/pi)
+    Ymax = vt*sqrt((Lt*At)/(g*As))
+    Hs = 1.2*(Hns+Ymax)
+    vs = Vdotn/As
+    sugretank_dict = sort((Dict("s. Diameter of surge tank"=>Ds,
+                    "t. Cross-section of surge tank"=>As,
+                    "u. Maximum height of water oscillation"=>Hns+Ymax,
+                    "v. Height of surge tank"=>Hs,
+                    "w. Allowed velocity of water inside surge tank"=>vs)))
+    return sort(merge(headrace_dict,surgetank_dict))
 end
